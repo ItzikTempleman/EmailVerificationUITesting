7faf1a065a8f109530f359d9b72adfd104104a6d
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-11-12 21:25:12 +0200

    d

--- app/src/main/java/com/itzik/user_with_testing/project/ui/screens/CreateAccountScreen.kt
+++ app/src/main/java/com/itzik/user_with_testing/project/ui/screens/CreateAccountScreen.kt
@@ -1,11 +1,12 @@
 package com.itzik.user_with_testing.project.ui.screens
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
@@ -16,6 +17,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,20 +53,25 @@ fun CreateAccountScreen(
         modifier = modifier.fillMaxSize()
     ) {
         val (backBtn,title) = createRefs()
-        Icon(
-            tint = Color.White,
-            contentDescription = null,
-            imageVector = Icons.Default.ArrowBack,
-            modifier = modifier.width(50.dp).height(50.dp)
-                .clickable {
-                    navHostController.navigate(LoginGraph.LoginPage.route)
-                }
-                .constrainAs(backBtn) {
+        OutlinedButton(
+            border = BorderStroke(1.2.dp, White),
+            shape = CircleShape,
+            contentPadding = PaddingValues(0.dp),
+            onClick = {
+                navHostController.navigate(LoginGraph.LoginPage.route)
+            },
+            modifier = modifier.size(80.dp).constrainAs(backBtn) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
-                }
-                .padding(top = 8.dp, start = 20.dp),
-        )
+            }.padding(20.dp),
+        ) {
+           Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = null,
+                tint = White
+            )
+        }
+
         Text(
             text = stringResource(id = R.string.create_new),
             modifier = modifier
@@ -72,8 +80,8 @@ fun CreateAccountScreen(
                     top.linkTo(backBtn.bottom)
                     end.linkTo(parent.end)
                 }
-                .padding(20.dp),
-            color = Color.White,
+                .padding(horizontal = 20.dp),
+            color = White,
             fontSize = 32.sp,
         )
 
@@ -119,7 +127,7 @@ fun CreateAccountScreen(
                     }, colors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Blue,
                         unfocusedBorderColor = Color.DarkGray,
-                        backgroundColor = Color.White
+                        backgroundColor = White
                     ),
                     leadingIcon = {
                         Icon(
--- app/src/main/java/com/itzik/user_with_testing/project/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/itzik/user_with_testing/project/ui/screens/LoginScreen.kt
@@ -1,20 +1,25 @@
 package com.itzik.user_with_testing.project.ui.screens
 
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.filled.Smartphone
@@ -26,6 +31,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -103,7 +110,7 @@ fun LoginScreen(
             ConstraintLayout(
                 modifier = Modifier.fillMaxSize()
             ) {
-                val ( emailTF, passwordTF, loginBtn, forgotPasswordText, iconRow, signUp, phoneNumberOutlinedTF, or) = createRefs()
+                val (emailTF, passwordTF, loginBtn, forgotPasswordText, iconRow, signUp, phoneNumberOutlinedTF, or, signUpBtn) = createRefs()
                 val context = LocalContext.current
 
                 var email by remember { mutableStateOf("") }
@@ -269,13 +276,12 @@ fun LoginScreen(
                         .constrainAs(forgotPasswordText) {
                             top.linkTo(loginBtn.bottom)
                             end.linkTo(parent.end)
+                            start.linkTo(parent.start)
                         }
                         .padding(start = 40.dp, end = 40.dp, top = 20.dp),
                     text = stringResource(id = R.string.forgot),
-                    color = Blue,
+                    color = Black,
                     fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic
                 )
 
                 if (isEnterPhoneNumberDisplayed) {
@@ -331,28 +337,42 @@ fun LoginScreen(
                         }
                         .padding(top = 160.dp),
                     text = "-----------------------  OR  ------------------------",
-                    color = Blue,
+                    color = Color.Black,
                     fontSize = 14.sp
                 )
 
                 Text(
                     modifier = modifier
-                        .clickable {
-                            navHostController.navigate(LoginGraph.CreateAccountPage.route)
-                        }
                         .constrainAs(signUp) {
                             top.linkTo(or.bottom)
                             end.linkTo(parent.end)
                             start.linkTo(parent.start)
                         }
-                        .padding(20.dp),
+                        .padding(vertical = 20.dp, horizontal = 8.dp),
                     text = stringResource(id = R.string.sign_up),
                     color = Blue,
                     fontSize = 20.sp,
                     fontStyle = FontStyle.Italic,
                     fontWeight = FontWeight.Bold
                 )
-
+                OutlinedButton(
+                    border = BorderStroke(1.2.dp, Blue),
+                    shape = CircleShape,
+                    contentPadding = PaddingValues(0.dp),
+                    onClick = {
+                        navHostController.navigate(LoginGraph.CreateAccountPage.route)
+                    },
+                    modifier = modifier.size(40.dp).constrainAs(signUpBtn) {
+                        top.linkTo(signUp.top)
+                        bottom.linkTo(signUp.bottom)
+                        start.linkTo(signUp.end)
+                    }.padding(2.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.ArrowForward,
+                        contentDescription = null,
+                        tint= Blue
+                        )
+                }
 
             }
         }
