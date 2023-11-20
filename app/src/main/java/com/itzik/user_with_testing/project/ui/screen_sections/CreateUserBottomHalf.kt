f67d6c5c84e2036533f02e01a88485492fd52b3e
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2023-11-20 17:21:18 +0200

    f
    f

---
+++ app/src/main/java/com/itzik/user_with_testing/project/ui/screen_sections/CreateUserBottomHalf.kt
@@ -0,0 +1,10 @@
+package com.itzik.user_with_testing.project.ui.screen_sections
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateUserBottomHalf(
+
+) {
+
+}
---
+++ app/src/main/java/com/itzik/user_with_testing/project/ui/screen_sections/CreateUserTopHalf.kt
@@ -0,0 +1,11 @@
+package com.itzik.user_with_testing.project.ui.screen_sections
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateUserTopHalf(
+
+
+) {
+
+}
